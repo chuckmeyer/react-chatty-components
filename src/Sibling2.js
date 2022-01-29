e236ff0d2a172ce09294d637b0fdb53c3c9ae3a6
Chuck Meyer <chuck.meyer@algolia.com>
2022-01-28 22:17:11 -0500

    Swapped render only components to functions

--- src/Sibling2.js
+++ src/Sibling2.js
@@ -1,14 +1,10 @@
-import React from 'react';
-
-class Sibling2 extends React.Component {
-  render() {
-    return (
-      <div className='sibling'>
-        <h1>Sibling 2</h1>
-        <h2>{ this.props.message ? this.props.message: 'Your ad here' }</h2>
-      </div>
-    );
-  }
+function Sibling2(props) {
+  return (
+    <div className='sibling'>
+      <h1>Sibling 2</h1>
+      <h2>{ this.props.message ? this.props.message: 'Your ad here' }</h2>
+    </div>
+  );
 }
 
 export { Sibling2 };
