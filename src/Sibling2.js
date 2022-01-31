9fccb20f82118a62a2354497c12ac87c9ad7b3cf
Chuck Meyer <chuck.meyer@algolia.com>
2022-01-31 10:25:12 -0500

    Fixed sibling2 props as functional component

--- src/Sibling2.js
+++ src/Sibling2.js
@@ -2,7 +2,7 @@ function Sibling2(props) {
   return (
     <div className='sibling'>
       <h1>Sibling 2</h1>
-      <h2>{ this.props.message ? this.props.message: 'Your ad here' }</h2>
+      <h2>{ props.message ? props.message: 'Your ad here' }</h2>
     </div>
   );
 }
